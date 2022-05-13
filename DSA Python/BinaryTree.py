aa832e4e6d952041c22f0558f44be05bd0054cf0
Ndongmo Christian <christianhonore2003@gmail.com>
2022-05-13 16:59:37 +0100

    Added a method to add an element to my Binary Tree

--- DSA Python/BinaryTree.py
+++ DSA Python/BinaryTree.py
@@ -1,16 +1,40 @@
-from ast import Sub
-
 
 class BinTree():
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
+
+    def add(self, item, pos):
+        if self.left == None or self.right is None:
+            new_root = BinTree(item)
+            if pos == 'left':
+                self.right = new_root
+            else:
+                self.left = new_root
+        else:
+            print('element cannot be added here')
+
+
+
+
 def preorder(SubTree):
     if SubTree is not None:
         print(f'{SubTree.data}')
         preorder(SubTree.left)
         preorder(SubTree.right)
+
+def inorder(SubTree):
+    if SubTree is not None:
+        preorder(SubTree.left)
+        print(f'{SubTree.data}')
+        preorder(SubTree.right)
+
+def postorder(SubTree):
+    if SubTree is not None:
+        preorder(SubTree.left)
+        preorder(SubTree.right)
+        print(f'{SubTree.data}')
 
 n5 = BinTree(5)
 n2 = BinTree(2)
@@ -32,4 +56,6 @@ n2.left = n3
 n4.right = n6
 n4.left = n7
 
-preorder(n5)
+
+n7.add(5, 'right')
+preorder(n4)
